af7ea63d2b3d42c52bd8191611615d4450943c82
danielkanangila <daniel.kanangila@gmail.com>
2020-08-11 21:47:46 -0400

    pass word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,36 @@
 def word_count(s):
     # Your code here
+    wList = s.split()
+    ignored_char = [":", ";", ",", ".", "-", "+", "=", "/", "\\",
+                    " | ", "[", "]", "{", "}", "(", ")", "*", " ^ ", " &", ]
 
+    d = {}
+
+    for word in wList:
+        if word in ignored_char:
+            continue
+        word = parse(word.lower())
+        if word != "":
+            if word not in d:
+                d[word] = 1
+            else:
+                d[word] += 1
+    return d
+
+
+def parse(s):
+    w = ""
+    for char in s:
+        if char == "'":
+            w += char
+        if char.isalnum():
+            w += char
+    return w
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
